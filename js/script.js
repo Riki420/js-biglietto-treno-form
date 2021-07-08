bf46b26435ad9382f4a5e71b73a8100028605e4c
Riki420 <girottiriccardo1998@gmail.com>
2021-07-08 16:09:48 +0200

    create variabili per le informazioni del form

--- js/script.js
+++ js/script.js
@@ -7,3 +7,20 @@
  L'output del prezzo finale va stampato in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 */
 
+//* Prendo le informazioni dall'utente e con un bottone faccio fare i calcoli allo script dando il risultato sullo schermo
+
+//*1 Prendo le informazioni
+//*2 Calcolo del prezzo a seconda dei km scelti
+//*3 A seconda dell'età genero lo sconto del 40 o 20 %
+//*4 Calcolo del prezzo finale arrotondandolo con due decimali
+//*5 Stampo i dati inseriti dall'utente dentro i propri spazi
+
+/!**************************************************************************************************************************************************/
+
+
+//*Variabili per il form
+
+var nameField = document.getElementById('name');
+var kmField = document.getElementById('km');
+var ageField = document.getElementById('age');
+var button = document.getElementById('gen-ticket');
